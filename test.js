36c2ae7b4b1dd06ffde54489a9b41e5668ef7ca6
tommy <tooto1985@gmail.com>
2016-06-20 11:36:14 +0800

    modify test.js

--- test.js
+++ test.js
@@ -1,13 +1,17 @@
 var isRunning = true;
 function movingTo(value) {
-    if (isRunning && value>document.body.scrollTop) {
+    if (isRunning && value > document.body.scrollTop) {
+        var step = 100;
+        var i = 100;
+        var max = 10000;
         setTimeout(function() {
             document.body.scrollTop = value;
-            console.log("[" + document.body.scrollTop + "," + value+"]");
-            if (value>document.body.scrollTop) {
-                setTimeout(arguments.callee,100);
+            console.log("[" + document.body.scrollTop + "," + value + "]");
+            if (value > document.body.scrollTop && i < max) {
+                setTimeout(arguments.callee, step);
+                i += step;
             }
-        },100);
+        }, step);
         isRunning = false;
     }
 }
