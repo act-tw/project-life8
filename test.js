9602c5ecaa6b78eddc28d50f7224f049e58d9f58
tommy <tooto1985@gmail.com>
2016-06-20 10:47:07 +0800

    modify test.js

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 var isRunning = true;
-
 function test(value) {
-    if (isRunning) {
-        console.log("[test]:" + document.body.scrollTop + "," + value);
+    if (isRunning && value>document.body.scrollTop) {
+        setTimeout(function() {
+            if (value>document.body.scrollTop) {
+                setTimeout(arguments.callee,500);
+            }
+            document.body.scrollTop = value;
+            console.log("[" + document.body.scrollTop + "," + value+"]");
+        },500);
         isRunning = false;
     }
 }
-
-console.log("do");
